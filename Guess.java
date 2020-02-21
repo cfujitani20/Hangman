2b3879e2a36865ef7bd1f68f7c06f1ad34e1036d
cfujitani20 <cfujitani20@punahou.edu>
2020-02-21 18:45:13 +0000

    First commit

---
+++ Guess.java
@@ -0,0 +1,12 @@
+// By Cade Fujitani
+public class Guess
+{
+    public static void dictionary
+    {
+         ArrayList<Words> dict = new 
+    }
+    public static void hang(String guess)
+    {
+
+    }
+}
---
+++ Hangman.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+// By Cade Fujitani
+public class Hangman
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Words> dict
+    }
+}
