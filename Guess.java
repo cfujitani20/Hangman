af60f38e391d3a017e2ccd4bb908c69af8008f26
cfujitani20 <cfujitani20@punahou.edu>
2020-02-25 18:40:40 +0000

    HangmanDriver.java

--- Guess.java
+++ Guess.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 // By Cade Fujitani
 public class Guess
 {
-    public static void dictionary
+    public dictionary()
     {
-         ArrayList<Words> dict = new 
+         // generate dictionary and arraylist
+         ArrayList<String> dict = new ArrayList<String>();
+         dict.add("goldfish, water, grapes, teeth, potato, computer, cellphone, backpack, affix, abyss, twyndyllyngs, syzygy ");
     }
     public static void hang(String guess)
     {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("What letter do you guess?");
 
     }
 }
