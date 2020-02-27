639cc010f22cffd0a0c8935e1569b255d446226a
cfujitani20 <cfujitani20@punahou.edu>
2020-02-27 15:12:01 +0000

    HangmanDriver.java

--- Guess.java
+++ Guess.java
@@ -3,16 +3,18 @@ import java.util.ArrayList;
 // By Cade Fujitani
 public class Guess
 {
-    public dictionary()
-    {
-         // generate dictionary and arraylist
-         ArrayList<String> dict = new ArrayList<String>();
-         dict.add("goldfish, water, grapes, teeth, potato, computer, cellphone, backpack, affix, abyss, twyndyllyngs, syzygy ");
-    }
-    public static void hang(String guess)
+    public static void hangman(char guess)
     {
         Scanner kb = new Scanner(System.in);
         System.out.println("What letter do you guess?");
+        for (int i = 0; i < guessWord.length(); i++)
+        {
+            if (secretWord.charAt(i) == guess.charAt(0))
+            {
+                String display += guess.charAt(0);
+            }
+            else if
+        }
 
     }
 }
--- HangmanDriver.java
+++ HangmanDriver.java
@@ -7,6 +7,7 @@ public class HangmanDriver
     {
         // generate dictionaryionary and arraylist
         ArrayList<String> dictionary = new ArrayList<String>();
+        // add each dictionary word manually
         dictionary.add("goldfish");
         dictionary.add("water");
         dictionary.add("grapes");
@@ -17,18 +18,30 @@ public class HangmanDriver
         dictionary.add("backpack");
         dictionary.add("syzygy");
         dictionary.add("twyndyllyngs");
+        // convert a random word from the arraylist into a blank word for guessing
         String guessWord = dictionary.get((int)(Math.random() * dictionary.size()));
+        String secretWord = guessWord;
+        // Arraylist<String> pastguesses = new ArrayList<String>();
+        // pastguesses.add(guess);
         String display = "";
+        hangman = new hangman();
         for (int i = 0; i < guessWord.length(); i++)
         {
             display += "_";
         }
+        // generate amount of attempts
         int attempts = 7;
+        Scanner kb = new Scanner(System.in);
         //make gamemode false until game is won
         boolean gameOver = false;
-        while (!gameOver)
+        while (!gameOver && attempts > 0)
         {
             System.out.println("Welcome to Hangman!");
+            System.out.println("Current guesses: " + attempts);
+            System.out.println("Enter a single character: ");
+            System.out.println(display);
+            char guess = kb.nextLine();
+            hangman(guess);
         }
     }
 }
